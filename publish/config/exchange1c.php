29d8b77c85cd830cb24a044aa4cbe178e80b84e8
Burtsev Alexander <Alexander.burzev@gmail.com>
2022-02-23 14:10:34 +0200

    chore(config): fixcodestyle

--- publish/config/exchange1c.php
+++ publish/config/exchange1c.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * This file is part of bigperson/laravel-exchange1c package.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 declare(strict_types=1);
 
 return [
@@ -27,7 +29,7 @@ return [
             if ($username == 'admin' && $password == 'admin') {
                 return true;
             }
-    
+
             return false;
         }
     ]
